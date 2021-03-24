a033ee15e78e223b6bc2204a6f240692e26cbee9
Ada Luong <ada.luong@student.unsw.edu.au>
2021-03-24 14:23:56 +1100

    fixed errors in file.c

--- kern/include/file.h
+++ kern/include/file.h
@@ -8,8 +8,10 @@
 /*
  * Contains some file-related maximum length constants
  */
+
 #include <limits.h>
-#define MAX_PROCESS 10
+
+#define MAX_PROCESS 128
 #define NUM_SYSCALLS 7
 #define OPEN 0
 #define READ 1
@@ -24,28 +26,28 @@
  */
 
 // open file table entry
-struct open_file {
-    FILE *fp;
-    struct vnode *v_ptr
+typedef struct open_file {
+    int *fp;
+    struct vnode *v_ptr;
 } of_entry;
 
 // open file table array
-struct open_file_table {
+typedef struct open_file_table {
     of_entry open_file_table[OPEN_MAX];
     int cur_index;
 } of_table;
 
 // file descriptor entry 
-struct fd_entry {
+typedef struct fd_entry {
     of_entry *process_fd_array[MAX_PROCESS];
     int cur_index;
 } fd_entry;
 
 /* HELPER FUNCTIONS */
 fd_entry *initialize_fd_table(void);
-of_entry *create_open_file(FILE *fp, struct vnode *v_ptr);
+of_entry *create_open_file(int *fp, struct vnode *v_ptr);
 of_table *create_of_table(void);
-int add_to_of_table(of_entry *ofptr, of_table of_table[]);
-int add_to_fd_table(int process, of_entry *process_fd_table[][], of_entry *ofptr);
+int add_to_of_table(of_entry *ofptr, of_table *of_table);
+int add_to_fd_table(int process, fd_entry *process_fd_table, of_entry *ofptr);
 
 #endif /* _FILE_H_ */
--- kern/syscall/file.c
+++ kern/syscall/file.c
@@ -23,9 +23,9 @@
 
 // file descriptor array | created per process | return the array (pointer to the array)
 fd_entry *initialize_fd_table(void) {
-    fd_entry per_process_fd_table[NUM_SYSCALLS];
+    fd_entry *per_process_fd_table = kmalloc((sizeof(of_entry) * NUM_SYSCALLS));
 
-    for (i = 0; i < NUM_SYSCALLS; i++) {
+    for (int i = 0; i < NUM_SYSCALLS; i++) {
         per_process_fd_table[i].cur_index = 3;
     }
 
@@ -33,8 +33,8 @@ fd_entry *initialize_fd_table(void) {
 }
 
 // create a new open file | return pointer to the open_file struct
-of_entry *create_open_file(FILE *fp, struct vnode *v_ptr) {
-    of_entry *new_open_file = malloc(sizeof(of_entry));
+of_entry *create_open_file(int *fp, struct vnode *v_ptr) {
+    of_entry *new_open_file = kmalloc(sizeof(of_entry));
     new_open_file->fp = fp;
     new_open_file->v_ptr = v_ptr;
 
@@ -42,18 +42,22 @@ of_entry *create_open_file(FILE *fp, struct vnode *v_ptr) {
 }
 
 of_table *create_of_table(void) {
-    of_table *new_of_table = malloc(sizeof(of_table));
+    of_table *new_of_table = kmalloc(sizeof(of_table));
     new_of_table->cur_index = 0;
 
     return new_of_table;
 }
 
 // add to the open file table / array | return success 0 or failure 1
-int add_to_of_table(of_entry *ofptr, of_table of_table[]) {
-    if (of_table->cur_index >= OPEN_MAX) return 1;
+int add_to_of_table(of_entry *ofptr, of_table *of_table) {
+
+    int current_index = of_table->cur_index;
+
+    if (current_index >= OPEN_MAX) return 1;
 
     /* ADD SYNCHRONISATION HERE */
-    of_table[of_table->cur_index] = of_entry;
+    of_table->open_file_table[current_index] = *ofptr;
+
     of_table->cur_index++;
     /*--------------------------*/
 
@@ -61,11 +65,16 @@ int add_to_of_table(of_entry *ofptr, of_table of_table[]) {
 }
 
 // add to a descriptor array | return success 0 or failure 1
-int add_to_fd_table(int process, fd_entry *process_fd_table[], of_entry *ofptr) {
-    if (process_fd_array[process].cur_index >= MAX_PROCESS) return 1;
+int add_to_fd_table(int process, fd_entry *process_fd_table, of_entry *ofptr) {
 
-    process_fd_table[process].process_fd_array[process_fd_array[process].cur_index] = ofptr;
-    process_fd_array[process].cur_index++;
+    int current_index = process_fd_table[process].cur_index;
+
+    if (current_index >= MAX_PROCESS) return 1;
+
+    process_fd_table[process].process_fd_array[current_index] = ofptr;
+
+    process_fd_table[process].cur_index++;
+
     return 0;
 }
 
